11c50b734010f685a67ef80a632ffea465cceada
Julia Pineda <Julia.Pineda@analog.com>
2021-12-04 17:22:43 +0800

    Upload solutions
    Signed-off-by: Julia Pineda <Julia.Pineda@analog.com>

---
+++ AoC2.m
@@ -0,0 +1,70 @@
+% input = {'forward 3','down 7','forward 8','down 9','forward 3','down 5','down 9','down 6','forward 1','forward 8','forward 9','forward 3','down 7','down 8','up 3','down 5','down 8','down 9','forward 5','forward 6','down 2','forward 1','forward 3','down 9','up 6','up 2','down 7','down 9','forward 2','down 9','up 3','forward 5','up 9','up 9','forward 4','down 6','up 7','up 8','up 6','up 2','down 7','forward 9','down 4','forward 2','forward 1','up 7','down 2','down 8','down 8','down 2','down 7','down 3','up 4','down 7','down 1','down 7','down 7','forward 8','forward 1','forward 5','forward 1','forward 9','forward 3','forward 5','down 3','forward 9','down 4','down 2','forward 5','down 4','down 4','down 4','down 9','down 6','down 5','forward 2','down 2','down 8','down 8','down 7','down 2','down 8','down 6','down 4','down 7','up 8','up 7','up 6','down 4','down 2','up 4','up 8','up 4','down 4','down 3','forward 7','forward 8','up 1','up 1','up 7','forward 2','down 2','forward 7','forward 7','forward 7','down 2','down 2','forward 5','down 3','forward 3','down 6','down 9','down 4','down 1','forward 2','forward 2','down 8','down 9','up 3','forward 8','down 7','forward 6','forward 6','down 9','up 2','up 9','down 2','down 8','up 2','forward 6','down 5','up 9','forward 6','down 3','down 9','forward 8','forward 8','forward 1','forward 6','down 7','forward 3','forward 5','forward 6','down 1','down 1','forward 3','forward 8','forward 8','up 5','down 6','up 5','down 1','up 3','down 6','forward 1','up 3','up 4','down 8','down 2','up 2','forward 1','forward 2','forward 6','up 2','down 3','up 5','down 5','down 7','up 6','up 6','down 8','forward 7','forward 2','forward 3','up 5','down 5','down 6','down 4','forward 8','up 1','forward 5','up 1','forward 4','forward 3','forward 4','down 2','up 5','up 4','down 5','forward 1','forward 8','down 7','down 1','down 7','forward 6','down 4','down 6','forward 6','down 8','forward 7','down 4','down 8','up 7','down 6','down 2','up 8','down 7','down 8','down 6','down 3','up 1','down 7','forward 1','down 3','down 5','down 9','down 3','forward 1','forward 4','forward 2','down 8','forward 7','down 3','down 7','forward 7','down 4','down 5','down 1','forward 2','down 7','forward 3','forward 4','down 8','up 6','down 4','up 5','forward 5','forward 7','up 8','forward 6','up 3','forward 9','up 9','down 1','down 2','forward 6','down 5','forward 6','down 7','forward 9','down 2','up 5','down 2','forward 3','up 7','forward 3','down 4','forward 7','forward 8','down 5','up 7','up 8','forward 7','down 4','forward 9','forward 6','down 2','forward 3','up 8','up 4','forward 2','forward 4','forward 1','down 6','forward 1','down 9','forward 2','up 5','down 2','down 5','down 5','down 2','down 9','down 4','forward 5','down 5','up 1','forward 4','down 6','down 5','up 8','up 4','down 8','forward 4','down 8','forward 4','down 2','down 8','forward 3','forward 7','down 1','forward 3','forward 7','forward 1','down 4','forward 2','down 3','down 4','forward 1','up 6','forward 8','down 5','up 6','up 3','forward 7','down 3','forward 4','forward 9','forward 2','up 2','up 7','forward 1','up 8','down 6','up 5','down 3','up 7','down 4','up 7','down 8','down 7','forward 5','up 7','down 1','up 9','down 2','down 8','down 5','forward 4','down 3','up 4','down 2','forward 6','down 2','forward 9','up 9','up 8','forward 3','forward 7','forward 7','up 4','up 3','up 4','forward 1','down 2','up 6','down 2','down 4','up 3','down 8','down 9','down 4','forward 6','down 5','down 5','forward 7','down 2','forward 8','up 2','up 7','down 5','down 7','up 9','up 4','up 5','down 4','down 8','down 9','down 7','down 1','down 1','up 7','up 8','down 5','forward 2','up 3','down 2','down 9','down 4','forward 1','forward 1','up 4','down 9','up 7','forward 7','up 8','up 4','down 2','down 1','forward 2','up 4','down 3','up 7','down 7','down 6','down 4','up 3','down 7','forward 5','down 6','down 9','down 3','forward 1','down 6','forward 3','down 5','up 1','forward 5','forward 4','forward 8','forward 1','up 4','down 2','down 7','down 9','up 4','up 6','forward 8','up 4','down 8','forward 7','forward 1','down 7','forward 9','forward 1','forward 7','forward 9','down 3','forward 4','down 3','down 3','down 3','down 1','down 5','forward 1','forward 7','up 5','forward 5','forward 4','forward 7','down 7','up 5','forward 1','forward 8','down 3','up 6','down 7','forward 6','down 8','forward 6','up 4','down 9','up 1','forward 4','forward 8','down 5','forward 7','up 9','forward 2','up 2','forward 7','down 3','forward 2','down 7','down 1','forward 9','forward 3','up 5','forward 7','up 4','forward 2','down 3','down 2','down 8','forward 7','down 8','down 3','forward 9','forward 9','forward 5','up 8','forward 9','up 2','forward 2','up 4','forward 2','forward 1','forward 7','forward 6','down 5','forward 5','forward 1','forward 7','forward 4','down 5','down 9','down 8','forward 5','up 5','up 1','down 8','down 9','forward 1','down 5','forward 6','down 1','down 7','down 7','down 4','forward 3','down 5','down 8','forward 2','down 4','forward 3','down 1','up 5','down 8','forward 9','up 6','up 4','down 6','down 9','forward 2','forward 5','down 3','forward 2','down 5','forward 1','forward 4','down 6','down 4','up 2','down 9','forward 2','up 8','down 4','forward 7','down 5','forward 8','down 1','forward 5','down 7','up 5','down 7','down 4','forward 3','down 1','forward 7','forward 9','up 9','forward 5','forward 8','up 8','forward 6','down 5','down 8','forward 9','forward 3','down 3','forward 9','forward 7','forward 6','forward 1','up 2','forward 8','down 9','down 5','down 5','down 4','forward 7','down 8','down 7','forward 8','forward 2','down 5','forward 2','forward 3','down 6','down 8','up 8','forward 7','down 7','up 7','forward 5','up 6','forward 7','up 6','forward 8','forward 5','up 6','forward 1','down 5','forward 1','up 5','down 1','forward 6','forward 3','up 2','forward 9','forward 8','down 9','forward 7','forward 7','forward 8','up 9','down 8','up 1','down 4','forward 2','forward 7','down 1','up 7','down 5','forward 2','down 4','down 8','forward 6','down 3','forward 8','up 6','forward 5','forward 7','down 5','down 1','down 7','down 7','up 8','down 5','forward 4','down 5','down 7','down 2','up 8','forward 6','up 1','down 3','forward 9','up 1','down 7','forward 2','down 6','forward 6','up 3','up 9','up 2','forward 4','forward 4','down 4','down 4','down 2','down 5','forward 7','forward 6','forward 9','down 9','forward 6','up 1','down 2','down 4','down 7','down 5','down 4','up 2','up 8','down 6','forward 2','up 7','down 2','up 5','down 8','forward 3','up 2','forward 3','forward 5','forward 9','forward 2','forward 8','forward 1','down 5','up 6','forward 9','forward 4','up 8','down 8','up 2','down 9','down 3','down 5','up 4','forward 2','down 5','forward 6','down 6','forward 2','forward 7','forward 6','forward 1','down 7','forward 8','up 2','forward 5','forward 5','up 3','forward 2','forward 3','forward 8','forward 4','down 1','down 9','up 5','down 5','forward 1','down 8','up 3','down 9','forward 5','forward 2','forward 2','up 1','forward 1','down 5','forward 4','up 4','up 3','up 3','down 4','down 5','down 1','forward 8','down 4','up 7','down 6','forward 5','forward 4','forward 8','forward 8','up 3','forward 7','down 4','forward 8','forward 4','forward 6','down 8','down 6','down 1','forward 4','down 8','down 6','forward 2','down 8','up 7','down 2','forward 5','down 4','down 7','down 8','forward 5','down 4','down 2','down 7','down 4','forward 7','forward 3','up 4','down 7','down 9','up 4','up 5','down 7','up 5','down 3','down 5','forward 8','up 1','up 9','forward 9','forward 7','down 2','up 1','up 7','down 3','forward 3','up 6','forward 1','forward 2','down 4','down 3','forward 4','forward 7','forward 9','down 8','down 6','forward 1','forward 3','forward 2','up 3','down 6','down 7','up 8','down 5','up 6','down 8','forward 4','up 3','forward 3','forward 4','down 5','up 5','down 4','down 5','down 5','forward 3','up 9','down 8','down 2','down 7','down 4','down 3','up 2','forward 6','down 4','down 1','down 4','forward 1','forward 8','down 3','down 7','down 3','forward 1','down 9','down 7','down 3','down 3','down 6','down 8','down 2','down 5','up 1','up 7','forward 9','forward 6','forward 7','forward 8','up 7','down 9','down 4','down 7','forward 2','forward 4','forward 1','forward 4','forward 3','forward 8','down 5','forward 8','up 4','up 9','forward 3','down 7','forward 9','down 2','forward 5','up 7','down 4','down 3','down 5','down 4','forward 9','forward 5','forward 6','forward 6','down 2','down 7','forward 4','up 5','down 8','down 9','forward 5','down 8','forward 9','up 6','forward 8','forward 7','up 7','down 9','forward 5','down 9','forward 7','down 5','down 7','forward 9','forward 4','up 1','up 9','forward 7','up 5','up 2','down 6','forward 7','down 2','forward 6','forward 7','down 4','up 3','up 9','forward 4','forward 6','forward 3','up 6','down 5','up 2','forward 3','down 5','forward 1','up 7','down 8','up 4','forward 9','forward 6','down 7','forward 4','down 9','down 8','down 7','forward 7','down 5','forward 3','up 5','forward 9','forward 7','forward 3','down 5','forward 8','down 3','forward 1','down 3','down 3','forward 2','down 4','forward 7','up 1','down 6','up 3','forward 8','down 6','forward 6','down 5','down 4','forward 3','up 1','forward 5','forward 5','forward 2','up 9','forward 3','down 5','up 3','up 4','down 7','down 4','up 3','up 3','forward 2','up 2','forward 8','down 9','down 1','up 8','down 2','up 4','forward 9','up 1','down 8','forward 3','up 9','down 7','down 5','down 9','down 1','down 2','down 9','down 4','down 8','down 8','down 9','down 2','down 6','down 9','forward 8'}';
+input = {'forward 5','down 5','forward 8','up 3','down 8','forward 2'}';
+[numInput,~] = size(input);
+hPosition = zeros(numInput,1);
+depth = zeros(numInput,1);
+for i = 1 : numInput
+    command = input{i};
+    if i > 1
+        switch command(1)
+            case 'd'
+                depth(i) = depth(i-1) + str2num(command(end));
+                hPosition(i) = hPosition(i-1);
+            case 'u'
+                depth(i) = depth(i-1) - str2num(command(end));
+                hPosition(i) = hPosition(i-1);
+            case 'f'
+                hPosition(i) = hPosition(i-1) + str2num(command(end));
+                depth(i) = depth(i-1);
+        end
+    elseif i == 1
+        switch command(1)
+            case 'd'
+                depth(i) = str2num(command(end));
+            case 'u'
+                depth(i) = str2num(command(end));
+            case 'f'
+                hPosition(i) = str2num(command(end));
+        end
+    end
+end
+
+puzzleAns1 = depth(end)*hPosition(end);
+disp("Puzzle 1 Answer: "+num2str(puzzleAns1));
+
+%% Part 2
+
+hPosition = zeros(numInput,1);
+depth = zeros(numInput,1);
+aim = zeros(numInput,1);
+for i = 1 : numInput
+    command = input{i};
+    if i > 1
+        switch command(1)
+            case 'd'
+                aim(i) = aim(i-1) + str2num(command(end));
+                hPosition(i) = hPosition(i-1);
+                depth(i) = depth(i-1);
+            case 'u'
+                aim(i) = aim(i-1) - str2num(command(end));
+                hPosition(i) = hPosition(i-1);
+                depth(i) = depth(i-1);
+            case 'f'
+                aim(i) = aim(i-1);
+                hPosition(i) = hPosition(i-1) + str2num(command(end));
+                depth(i) = depth(i-1) + (aim(i-1)*str2num(command(end)));
+        end
+    elseif i == 1
+        switch command(1)
+            case 'd'
+                aim(i) = str2num(command(end));
+            case 'u'
+                aim(i) = str2num(command(end));
+            case 'f'
+                hPosition(i) = str2num(command(end));
+        end
+    end
+end
+
+puzzleAns2 = depth(end)*hPosition(end);
+disp("Puzzle 2 Answer: "+num2str(puzzleAns2));
