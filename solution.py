5bdb2958e9436efcab2a0227d85c3a74bdba4124
Pineda <Julia.Pineda@analog.com>
2025-03-27 14:45:24 +0800

    Set puzzel a as default puzzle to solve
    Signed-off-by: Pineda <Julia.Pineda@analog.com>

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
 import common
 import argparse
+import re
 
 def day_1_puzzle_a(filename):
     input = common.read_file(filename)
@@ -64,6 +65,39 @@ def day_2_puzzle_b(input_matrix):
                 made_safe[i] = 1
     return sum(made_safe)
 
+def day_3_puzzle_a(filename):
+    input = common.read_file(filename)
+    # Combine all the lines into a single string
+    input = "".join(input)
+    # Use regex get substrings that match three patterns: mul(d,d) where d is a digit, do(), or don't()
+    valid_inst = re.findall(r"mul\(\d+,\d+\)|do\(\)|don't\(\)", input)
+    # Get the digits from the substrings in nx2 matrix if the string begings with mul, else return [1,0] if the string starts with do and [0,0] with the string starts with don't
+    valid_num = [re.findall(r"\d+", x) if x.startswith("mul") else [0,0] for x in valid_inst]
+    proceed = [[1,1] if x.startswith("do()") else [1,0] if x.startswith ("don't()") else [0,0] for x in valid_inst]
+    # Convert the strings to integers
+    valid_num = [[int(x) for x in y] for y in valid_num]
+    # Get multiplication of the two numbers in each row
+    answer = sum([x[0]*x[1] for x in valid_num])
+    return answer, [valid_num, proceed]
+
+def day_3_puzzle_b(input_matrix):
+    # Get first two columns of the input matrix
+    valid_num = input_matrix[0]
+    proceed = input_matrix[1]
+    do_mult = True
+    answer = 0
+    for i, pair in enumerate(valid_num):
+        prod = pair[0]*pair[1]
+        # Check if do() [1,0]
+        if proceed[i] == [1,1]:
+            do_mult = True
+        # Check if proceed
+        elif proceed[i] == [1,0]:
+            do_mult = False
+        if do_mult:
+            answer = answer + prod
+    return answer
+
 
 def main(day, mode="sample", puzzle="both"):
     if mode == "sample":
@@ -83,7 +117,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run Advent of Code solution.")
     parser.add_argument("--day", type=str, default="1", help="Day of the Advent of Code challenge")
     parser.add_argument("--mode", type=str, default="sample", help="Mode to run the solution (sample or input)")
-    parser.add_argument("--puzzle", type=str, default="both", help="Puzzle to solve (a or b)")
+    parser.add_argument("--puzzle", type=str, default="a", help="Puzzle to solve (a or b)")
 
     args = parser.parse_args()
     main(day=args.day, mode=args.mode, puzzle=args.puzzle)
